d58a484dabbb605a5ba0aecf1fa748dbcf780aca
aditya <dev@chimeracoder.net>
2012-03-25 06:38:02 -0400

    Removed encode-url-params, as it can be done using drakma

--- squarelisp.lisp
+++ squarelisp.lisp
@@ -3,7 +3,6 @@
 (ql:quickload "ironclad")
 (ql:quickload "alexandria")
 
-
 (load "./bootstrap.lisp")
 
 (defun authorize-uri (client-id client-secret redirect-uri)
@@ -15,17 +14,8 @@
 (defun auth-param (authenticator)
   authenticator)
 
-
 (defun set-token (client-id client-secret redirect-uri code)
   (concatenate 'string "https://foursquare.com/oauth2/access_token?client_id=" client-id "&client_secret=" client-secret "&grant_type=authorization_code&redirect_uri=" redirect-uri "&code=" code ))
-
-(defun encode-url-params (params)
-    "Encode the specified parameter alist as GET values (with Toodledo's format)"
-      (let ((key (caar params))
-                    (value (cdar params)))
-            (if (and key value)
-                    (concatenate 'string (string key) "=" (string value) ";" (encode-url-params (cdr params)))
-                          'NIL )))
 
 (defun query (authenticator endpoint &optional extra-fields params)
   (let* ((uri
